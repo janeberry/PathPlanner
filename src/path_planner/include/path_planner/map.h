e1852ba48eaab1b5614715d6a4ec9ce6e504df28
janeberry <janeuarykim@gmail.com>
2025-08-10 20:56:10 -0700

    not able to display the map but build succeed

--- src/path_planner/include/path_planner/map.h
+++ src/path_planner/include/path_planner/map.h
@@ -1,17 +1,23 @@
 #ifndef PATH_PLANNER_MAP_H
 #define PATH_PLANNER_MAP_H
 
-//ros
 #include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/occupancy_grid.hpp>
+#include <opencv2/opencv.hpp>
 
 class Map : public rclcpp::Node {
 public :
     Map();
     ~Map();
 
+    bool loadFromFile(const std::string & path);
+    void publishMap();
+
 private :
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr map_publisher_;
-    
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_publisher_;
+    std::vector<std::vector<int>> grid_;
+    double resolution_ = 0.05;          // meters/cell
+    geometry_msgs::msg::Pose origin_;   // map origin in world
 };
 
 #endif // PATH_PLANNER_MAP_H
--- src/path_planner/src/map.cpp
+++ src/path_planner/src/map.cpp
@@ -4,16 +4,76 @@
 
 #include "path_planner/map.h"
 #include <cstdio>
+#include <iostream>
 
 //Constructor
 Map::Map() : Node("map")
 {
- RCLCPP_INFO(this->get_logger(), "Map node is starting."); 
- 
- map_publisher_ = this->create_publisher<std_msgs::msg::String>("map_topic", 10);
+
+  RCLCPP_INFO(this->get_logger(), "Map Node Running"); 
+  
+  loadFromFile("/home/parallels/dev/project/PathPlanner/src/path_planner/map.pgm");
+
+  origin_.position.x = 0.0;
+  origin_.position.y = 0.0;
+  origin_.position.z = 0.0;
+  origin_.orientation.w = 1.0;
+
+  map_publisher_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("map", 10); 
+  publishMap();
+
 }
 
 Map::~Map() = default;
+
+
+bool Map::loadFromFile(const std::string & path)
+{
+  cv::Mat img = cv::imread(path, cv::IMREAD_GRAYSCALE);
+  if (img.empty()) {
+    RCLCPP_ERROR(this->get_logger(), "Failed to load map image from %s", path.c_str());
+    return false;
+  }
+
+  grid_.resize(img.rows, std::vector<int>(img.cols, 0));
+
+  for (int i = 0; i < img.rows; ++i) {
+    for (int j = 0; j < img.cols; ++j) {
+      int pixel_value = img.at<uchar>(i, j);
+      if (pixel_value == 255) {
+        grid_[i][j] = 0; // free space
+      } else if (pixel_value == 0) {
+        grid_[i][j] = 100; // occupied space
+      } else {
+        grid_[i][j] = -1; // unknown space
+      }
+    }
+  }
+
+  return true;
+}
+void Map::publishMap()
+{
+  nav_msgs::msg::OccupancyGrid map_msg;
+  map_msg.header.frame_id = "map";
+  map_msg.header.stamp = this->now();
+  
+  map_msg.info.resolution = resolution_;
+  map_msg.info.width = grid_[0].size();
+  map_msg.info.height = grid_.size();
+  
+  map_msg.info.origin = origin_;
+
+  map_msg.data.reserve(map_msg.info.width * map_msg.info.height);
+  // Fill the data array
+  for (const auto& row : grid_) {
+    for (const auto& cell : row) {
+      map_msg.data.push_back(cell);
+    }
+  }
+
+  map_publisher_->publish(map_msg);
+}
 
 
 
