4514433adec714b7b447f849b9d554bbb6f98dc0
janeberry <janeuarykim@gmail.com>
2025-08-10 17:38:12 -0700

    first build success for map node

--- src/path_planner/src/map.cpp
+++ src/path_planner/src/map.cpp
@@ -1,13 +1,29 @@
-// A representation of the map (a 2D array, a flatmap, bytes, etc)
+/** MAP
+ * *A representation of the map (a 2D array, a flatmap, bytes, etc)
+ */
 
-
+#include "path_planner/map.h"
 #include <cstdio>
+
+//Constructor
+Map::Map() : Node("map")
+{
+ RCLCPP_INFO(this->get_logger(), "Map node is starting."); 
+ 
+ map_publisher_ = this->create_publisher<std_msgs::msg::String>("map_topic", 10);
+}
+
+Map::~Map() = default;
+
+
+
+
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world path_planner package\n");
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<Map>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
 }
